8710e433d93672533ec6dc848b85b5e0fcd7d8db
Michael Williamson <mike@zwobble.org>
2019-06-16 10:43:35 +0100

    Make variables argument optional

--- chembl_graphql/graphql.py
+++ chembl_graphql/graphql.py
@@ -6,6 +6,6 @@ from .graph import create_graph, Query
 _executor = graphlayer.graphql.executor(query_type=Query)
 
 
-def execute(query, *, session, variables):
+def execute(query, *, session, variables=None):
     graph = create_graph(session=session)
     return _executor(query, graph=graph, variables=variables)
